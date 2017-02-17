7f27e89fd06534fb8bbcad914b52736b606414a1
katbtr <bisztrayk@gmail.com>
2017-02-17 10:06:58 +0100

    Final exam - modified

--- main.cpp
+++ main.cpp
@@ -11,8 +11,13 @@ vector<char> commonLetters(string str1, string str2) {
   vector<char> result;
   for (unsigned int i = 0; i < str1.length(); i++) {
     for (unsigned int j = 0; j < str2.length(); j++) {
-      if (tolower(str1[i]) != tolower(str1[i + 1]) && tolower(str1[i]) == tolower(str2[j]) && tolower(str1[j]) != tolower(str1[j + 1])) {
-        result.push_back(str1[i]);
+      if (tolower(str1[i]) == tolower(str2[j])) {
+        if (result.size() == 0) {
+          result.push_back(str1[i]);
+        }
+        else if (result.back() != str1[i]) {
+          result.push_back(str1[i]);
+        }
       }
     }
   }
@@ -23,8 +28,8 @@ int main()
 {
   string str1, str2;
   vector<char> common;
-  str1 = "Hello";
-  str2 = "World";
+  str1 = "Hellooooo";
+  str2 = "Worlllld";
 
   common = commonLetters(str1, str2);
 
